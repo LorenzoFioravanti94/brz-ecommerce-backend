99ae2ff8498e6af3c979623426dee337be718157
LorenzoFioravanti94 <lorenzo.frv@gmail.com>
2026-06-07 17:54:20 +0200

    applied city names cure to sellers and created Intermediate model

--- brz_ecommerce_backend/models/3_silver/1_staging/slv_stg_olist__sellers.sql
+++ brz_ecommerce_backend/models/3_silver/1_staging/slv_stg_olist__sellers.sql
@@ -10,5 +10,5 @@ WITH sellers AS (
 SELECT trim(seller_id) AS seller_id,
        seller_zip_code_prefix AS zip_code_prefix,
        upper(trim(seller_city)) AS city,
-       upper(trim(seller_state)) AS state
+       upper(trim(seller_state)) AS state_id
 FROM sellers
